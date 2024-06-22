be62100f818f12fae99ab499b73cdacc9b9d65d3
--unset-all <metalljan@yandex.ru>
2024-06-22 12:43:14 +0300

    create commit

---
+++ src/main/java/koschei/models/Needle7.java
@@ -0,0 +1,11 @@
+package koschei.models;
+
+public class Needle7 {
+    public Needle7() {
+    }
+
+    @Override
+    public String toString() {
+        return ", смерть Кощея на игле :( " + "";
+    }
+}
